1b2cb7175e8c6ef781c6a1a913e85573652a592f
Wolfy <s.leheup@gmail.com>
2017-03-13 21:35:36 +0100

    fin code + bonus

--- bn.js
+++ bn.js
@@ -1,6 +1,8 @@
 var $canon = $('#canon');
 var history = '';
 var myName = $("#moi").val();
+var b = 0;
+var v = 0;
 
 // Met à jour le message affiché à l´adversaire à chaque nouvelle
 // lettre saisie dans l´input ´Mon nom´
@@ -14,11 +16,52 @@ $("#moi").keyup(function(event) {
 $('#canon').bind("enterKey", function(e) {
 
     var id = $canon.val();
-    console.log(id);
+    var colorDiv = $('#' + id).css('background-color');
+    var boat = $('#' + id).text();
+
+
     $('#' + id).addClass("colorred");
 
     let fullSentence = promptMessage(myName) + id + '<br/>';
     $("#history").append(fullSentence);
+
+    if (colorDiv == 'rgb(0, 0, 255)' && boat === '') {
+        $("#history").append("PLOUF <br/>");
+    }
+
+    if (boat == 'B' && $('#' + id).hasClass("colorred")) {
+        $("#history").append("TOUCHE <br/>");
+        b += 1;
+        console.log(b);
+        if (b == 3) {
+            $("#history").append("COULE <br/>");
+        }
+    }
+
+    if (boat == 'V' && $('#' + id).hasClass("colorred")) {
+        $("#history").append("TOUCHE <br/>");
+        v += 1;
+        console.log(b);
+        if (v == 3) {
+            $("#history").append("COULE <br/>");
+        }
+    }
+
+    if (b == 3 && v == 3) {
+        $("#history").append("WIN <br/>");
+         $('#no-mans-land').append('<code>Consolation prize : click me ;)</code>');
+        $("#no-mans-land").on("click", function() {
+            $(this).css({
+              // 'background-image' : 'url("trollinwolf.jpg"',
+              'background-image' : 'url("http://25.media.tumblr.com/tumblr_m7swa6z5bb1rn95k2o1_400.gif")',
+              'height' : '530px',
+              'width' : '620px',
+              'margin-left' : '200px'
+             });
+        });
+    }
+
+
 
 });
 
